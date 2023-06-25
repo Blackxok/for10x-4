8b83569d4bdda9627d31e4ac0c5670231198dbeb
Blackxok <nasridoni1@gmail.com>
2023-06-25 14:12:07 +0500

    cng js

---
+++ components/main.js
@@ -0,0 +1,7 @@
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    let speed = document.getElementById('speed');
+    let pitch = document.getElementById('pitch');
+})
